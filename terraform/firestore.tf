de80dae981e930ef21abbdef8d92841d4c99f47a
Oscar Pulido <oscarpulido@google.com>
2024-10-19 13:34:18 -0500

    Demo install instructions, firestore db

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -16,7 +16,7 @@
 
 resource "google_firestore_database" "database" {
   project     = var.project
-  name        = "aef_database"
+  name        = "aef-database"
   location_id = var.region
   type        = "FIRESTORE_NATIVE"
 }
