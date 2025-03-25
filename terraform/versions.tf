1985998ff1f6201aedf1fdffbd3e985e4d66ce6e
Oscar Pulido <oscarpulido@google.com>
2025-03-25 15:45:23 -0500

    new tf version req

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 terraform {
-  required_version = ">= 1.7.4"
+  required_version = ">= 1.10.2"
   required_providers {
     google = {
       source  = "hashicorp/google"
